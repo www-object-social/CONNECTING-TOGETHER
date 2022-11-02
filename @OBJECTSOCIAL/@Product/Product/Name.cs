2d613de15ff095818b59f5c87dd5a513b8ec511d
Pirasath <connect@object.social>
2022-11-02 22:55:11 +0100

    Update

--- @OBJECTSOCIAL/@Product/Product/Name.cs
+++ @OBJECTSOCIAL/@Product/Product/Name.cs
@@ -1,4 +1,8 @@
 ﻿namespace Product;
+/// <summary>
+/// Made to reflect which product name is used
+/// Use the Product.Information.cs
+/// </summary>
 public enum Name
 {
     ForwardOBJECTSOCIAL=0,OBJECTSOCIAL=1,MemoryClaims=2,GoodClaims=3,BadClaims=4
