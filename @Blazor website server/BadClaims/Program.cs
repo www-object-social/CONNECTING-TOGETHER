744af3fbf6da199edb16fbae501dceaef019cf05
Pirasath <connect@object.social>
2022-11-02 20:47:30 +0100

    Update

---
+++ @Blazor website server/BadClaims/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseWebAssemblyDebugging();
+else
+    app.UseHsts();
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+app.Run();
---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# CONNECTING-TOGETHER BY OBJECT:SOCIAL Developer team.
+
+
+
+# Bad|Claims
+## @Blazor website client/BadClaims.Client
+### _Imports.razor
+[+ using UI](###-UI)
+### App.razor
+
+#CONNECTING TOGETHER
+## @UI
+### UI
+#### MainLayout
+
+
+
